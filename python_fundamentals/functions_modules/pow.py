ee722e0374fbe3aa8aaaa2db63b93695f05ccca7
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-09-19 21:03:20 +0300

    edit code

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 def pow(a, b):
     result = 1
-    b = abs(b)
-    for i in range(b):
-        result = result * a
-    return result
+    if b < 0:
+        for i in range(-b):
+            result = result * a
+        return 1 / result
+    else:
+        for i in range(b):
+            result = result * a
+        return result
