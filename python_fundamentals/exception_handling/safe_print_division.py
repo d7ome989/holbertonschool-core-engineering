36ce90a8ee536b89c6833b41cbe8952ee5341b3e
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-09-22 03:03:37 +0300

    add to print / 0

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except Exception:
-        print("Inside result: None")
+        pass
     finally:
         print("Inside result: {}".format(result))
     return result
