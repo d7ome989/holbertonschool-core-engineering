f9f39a15489edf0539544cd2e903967d3019a240
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-09-24 18:48:59 +0300

    add dsciption

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""Module that defines a class Square with a private size."""
 
 
 class Square:
+    """A class that represents a square."""
+
     def __init__(self, size):
+        """Initialize a square with a given size."""
         self.__size = size
