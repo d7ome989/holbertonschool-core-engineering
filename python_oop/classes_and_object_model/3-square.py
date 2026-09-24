2388704a37b6fcb36ad21c6f212d6b0122158113
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-09-24 19:07:13 +0300

    this is to get square

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module that defines a class Square with a private size."""
+
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with a given size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
