e44e0a201882d162b300718fec6f36d534343557
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-09-26 19:47:56 +0300

    animals sound and README file

---
+++ python_oop/abc/animals.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Module that defines an abstract Animal class and its subclasses."""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract base class representing an animal."""
+
+    @abstractmethod
+    def sound(self):
+        """Return the sound made by the animal."""
+        pass
+
+
+class Dog(Animal):
+    """Represent a dog."""
+
+    def sound(self):
+        """Return the sound made by a dog."""
+        return "Bark"
+
+
+class Cat(Animal):
+    """Represent a cat."""
+
+    def sound(self):
+        """Return the sound made by a cat."""
+        return "Meow"
