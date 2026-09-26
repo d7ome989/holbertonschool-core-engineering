08309084cc1815ee8cd850ac3005a095ad141d6a
d7ome989 <d.7.o.m.e.989@gmail.com>
2026-09-26 18:56:19 +0300

    this  is to check and base geometry

---
+++ python_oop/inheritance/base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module that defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Base class for geometric shapes."""
+
+    def area(self):
+        """Calculate the area."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
